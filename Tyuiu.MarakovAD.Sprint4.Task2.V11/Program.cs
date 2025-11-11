af17157534d17d9d95ff8757931bcbd33e997103
Андрей Мараков <Tyuiu.MarakovAD@outlook.com>
2025-11-11 19:44:46 +0500

    Task2

---
+++ Tyuiu.MarakovAD.Sprint4.Task2.V11/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Tyuiu.MarakovAD.Sprint4.Task2.V11.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #3 | Выполнил: Мараков А. Д. | ИБКСб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы c массивами                                 *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #11                                                             *");
+        Console.WriteLine("* Выполнил: Мараков Андрей Дмитриевич | ИБКСб-25-1                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан одномерный целочисленный массив на 15 элементов заполненный " +
+            "случайными в диапазоне от 3 до 8 подсчитать сумму четных элементов массива..");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Random rnd = new Random();
+        int len;
+        Console.WriteLine("Количество элементов массива: ");
+        len = Convert.ToInt32(Console.ReadLine());
+        int[] array = new int[len];
+        for (int i = 0; i <= len - 1; i++)
+        {
+            array[i] = rnd.Next(3, 9);
+        }
+        Console.WriteLine();
+        Console.WriteLine("Массив: ");
+        for (int i = 0; i <= len - 1; i++)
+        {
+            Console.Write(array[i] + "\t");
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        int sum = ds.Calculate(array);
+        Console.WriteLine(sum);
+        Console.ReadKey();
+    }
+}
